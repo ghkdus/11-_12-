c3251a4ec78d984d71fb25819b2d8aab7371b498
ajh020826 <100890662+ajh020826@users.noreply.github.com>
2022-06-03 21:14:15 +0900

    Update naverFinance.py

--- naverFinance.py
+++ naverFinance.py
@@ -33,3 +33,15 @@ with open('top6.csv', 'w') as fp:
   for i in range(6):
     line=f'{top[i]}'
     fp.write(line)
+
+#top6 뉴스 중 뉴스 골라보기
+select = list(map(int, input('몇 번째 뉴스를 골라올까요?(띄워쓰기로 숫자를 구분하세요)').split()))
+
+for i in (select):
+  line=f'{top[i-1]}'
+  print(line)
+
+with open('top6_select.csv', 'w') as fp:
+  for i in range(len(select)):
+    line=f'{top[select[i]-1]}\n'
+    fp.write(line)
